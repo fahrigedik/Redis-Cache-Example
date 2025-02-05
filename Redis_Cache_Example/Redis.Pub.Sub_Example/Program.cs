1312f77eb6071a7cae92f4b9edfbebf9d35a27f6
Fahri Gedik <fahrican9978@gmail.com>
2025-02-05 12:54:28 +0300

    Pub/Sub - Message Broker

---
+++ Redis_Cache_Example/Redis.Pub.Sub_Example/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using StackExchange.Redis;
+
+Console.WriteLine("Hello, World!");
+
+ConnectionMultiplexer redis = await ConnectionMultiplexer.ConnectAsync("localhost:6379");
+
+ISubscriber subscriber = redis.GetSubscriber();
+
+
+while (true)
+{
+    //await subscriber.PublishAsync("messages", "hello, world!");
+    await subscriber.PublishAsync("amazon.elma", "Amazon Pattern :");
+}
+
---
+++ Redis_Cache_Example/Redis.Pub.Sub_SubscribeExample/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using StackExchange.Redis;
+
+Console.WriteLine("Hello, World!");
+
+ConnectionMultiplexer redis = await ConnectionMultiplexer.ConnectAsync("localhost:6379");
+
+ISubscriber subscriber = redis.GetSubscriber();
+
+await subscriber.SubscribeAsync("messages", (channel, message) =>
+{
+    Console.WriteLine((string)message);
+});
+
+
+await subscriber.SubscribeAsync("amazon.*", (channel, message) =>
+{
+    Console.WriteLine((string)message);
+});
+
+Console.ReadLine();
